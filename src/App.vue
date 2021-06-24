648f35ae1b7e25c2ee58af0f8b30adcf67818871
Veronica <veronica.alcala@gmail.com>
2021-06-24 15:42:48 +0200

    hyrule jobs

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="app">
+    <header>
+      <div class="title">
+        <h1>Hyrule Jobs</h1>
+      </div>
+      <div class="order">
+        <button @click="handleClick('title')">Order by title</button>
+        <button @click="handleClick('salary')">Order by salary</button>
+        <button @click="handleClick('location')">Order by location</button>
+      </div>
+    </header>
+
+
+    <JobList :jobs="jobs" :order="order"/>
+  </div>
+</template>
+
+<script lang="ts">
+
+import { defineComponent, reactive, toRefs, ref } from 'vue';
+import Job from './types/Job'
+import OrderTerm from './types/OrderTerm'
+import JobList from './components/JobsList.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: { JobList },
+  setup() {
+    const jobs = ref<Job[]>([
+      { title: 'farm worker', location: 'lon lon ranch', salary: 30000, id: '1'},
+      { title: 'quarryman', location: 'death mountain', salary: 40000, id: '2'},
+      { title: 'flute player', location: 'the lost woods', salary: 35000, id: '3'},
+      { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4'},
+      { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5'}
+    ])
+
+    const order = ref<OrderTerm>('title')
+
+    const handleClick = (term: OrderTerm) => {
+      order.value = term
+    }
+
+    return {jobs, handleClick, order}
+  }
+});
+</script>
+
+<style>
+header {
+  text-align: center;
+}
+
+header .order {
+  margin-top: 20px;
+}
+
+button {
+  margin: 0 10px;
+  color: #11c96d;
+  border: 3px solid #11c96d;
+  background: #c9eedb;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+}
+</style>
---
+++ src/components/JobsList.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="job-list">
+        <p>Ordered by {{ order }}</p>
+        <transition-group name="list" tag="ul">
+            <li v-for="job in orderedJobs" :key="job.id">
+                <h2>{{job.title}} in {{job.location}}</h2>
+                <div class="salary">
+                    <img src="../assets/rupeeicon.png" alt="rupee icon">
+                    <p>{{job.salary}} rupees</p>
+                </div>
+                <div class="description">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis autem perferendis sunt nam quas animi quod fugit deserunt sit dolor, aspernatur possimus reprehenderit eius quos voluptate soluta. Error, ab architecto?</p>
+                </div>
+            </li>
+        </transition-group>
+    </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, PropType } from 'vue'
+import Job from '@/types/Job'
+import OrderTerm from '@/types/OrderTerm'
+
+export default defineComponent({
+    props: {
+        jobs: {
+            required: true,
+            type: Array as PropType<Job[]>
+        },
+        order: {
+            required: true,
+            type: String as PropType<OrderTerm>
+        }
+    },
+    setup(props) {
+        const orderedJobs = computed(() => {
+            return [...props.jobs].sort((a: Job, b: Job) => {
+                return a[props.order] > b[props.order] ? 1 : -1
+            })
+        })
+
+        return {orderedJobs}
+    }
+
+})
+</script>
+
+<style scoped>
+.job-list {
+    max-width: 960px;
+    margin: 20px auto;
+}
+
+.job-list ul {
+    padding: 0;
+}
+
+.job-list li {
+    list-style-type: none;
+    background: white;
+    padding: 16px;
+    margin: 16px 0;
+    border-radius: 4px;
+}
+
+.job-list h2 {
+    margin: 0 0 10px;
+    text-transform: capitalize;
+}
+
+.salary {
+    display: flex;
+}
+
+.salary img {
+    width: 30px;
+}
+
+.salary p {
+    color: #11c96d;
+    font-weight: bold;
+    margin: 10px 4px;
+}
+
+.list-move {
+    transition: all 1s;
+}
+</style>
